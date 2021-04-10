8f8026a9d26c41cfabf8dcccb53f1c2888b2e7f7
Mayel <git@mayel.space>
2021-04-10 16:07:11 +0200

    clean

--- mix.exs
+++ mix.exs
@@ -9,17 +9,17 @@ defmodule Bonfire.Data.Projects.MixProject do
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       description: "Projects",
-      homepage_url: "https://github.com/bonfire-ecosystem/bonfire_data_projects",
-      source_url: "https://github.com/bonfire-ecosystem/bonfire_data_projects",
+      homepage_url: "https://github.com/bonfire-networks/bonfire_data_projects",
+      source_url: "https://github.com/bonfire-networks/bonfire_data_projects",
       package: [
         licenses: ["MPL 2.0"],
         links: %{
-          "Repository" => "https://github.com/bonfire-ecosystem/bonfire_data_projects",
+          "Repository" => "https://github.com/bonfire-networks/bonfire_data_projects",
           "Hexdocs" => "https://hexdocs.pm/bonfire_data_projects",
         },
       ],
       docs: [
-        main: "readme", # The first page to display from the docs 
+        main: "readme", # The first page to display from the docs
         extras: ["README.md"], # extra pages to include
       ],
       deps: Mess.deps [ {:ex_doc, ">= 0.0.0", only: :dev, runtime: false} ]
